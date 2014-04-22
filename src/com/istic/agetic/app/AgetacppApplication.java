29557c450d77c7b8ba17d9a215158c4af72a1706
Mathieu <mathieu.rolland14270@orange.fr>
2014-04-22 17:21:13 +0200

    [Instanciaition] Création de l'application agectacpp héritant du Framework

---
+++ src/com/istic/agetic/app/AgetacppApplication.java
@@ -0,0 +1,14 @@
+package com.istic.agetic.app;
+
+import com.istic.sit.framework.application.FrameworkApplication;
+
+import android.widget.FrameLayout;
+
+public class AgetacppApplication extends FrameworkApplication {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+	
+}

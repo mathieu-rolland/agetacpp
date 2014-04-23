fd61b2ccdf06503e4411f72798d8fdb77f9c740d
anthonylemee <anthonylemee@gmail.com>
2014-04-23 09:57:31 +0200

    [dev] Task 34 - Modèle - Fragment page de demande de moyen.

---
+++ src/com/istic/agetac/fragments/DemandeDeMoyensFragment.java
@@ -0,0 +1,14 @@
+package com.istic.agetac.fragments;
+
+import android.app.Fragment;
+
+/**
+* Classe DemandeDeMoyensFragment : affiche la fenêtre de demande des moyens et permet de créer une liste de demandes de moyens
+* et de la soumettre ensuite.
+* 
+* @author Anthony LE MEE - 10003134
+*/
+public class DemandeDeMoyensFragment extends Fragment {
+	
+	
+}// Class DemandeDeMoyensFragment
